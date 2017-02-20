180afce8c121ca96e82faee20ab35a85251b36b9
josepharunjoseph <prasanna@prasannas-mac-mini.local>
2017-02-20 13:12:55 +0530

    Commit Map class.

---
+++ GitDemo/MapForItenerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForItenerary.m
+//  GitDemo
+//
+//  Created by prasanna on 2/20/17.
+//  Copyright © 2017 Ashadeep. All rights reserved.
+//
+
+#import "MapForItenerary.h"
+
+@implementation MapForItenerary
+
+@end
